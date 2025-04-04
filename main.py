ba722907b203ac2182442641a42ece60eb4f0c4f
lastcb <wmwielgosz@gmail.com>
2025-04-04 15:42:16 -0500

    ch3_report l2_arguments

--- main.py
+++ main.py
@@ -4,25 +4,30 @@ from stats import (
     sort_alpha_char_count
 )
 
+import sys
+
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_word_count(text)
-    characters_dict = get_characters_count(text)
-    alpha_char_count = sort_alpha_char_count(characters_dict)
-
-
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book_path}...")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-    for c in alpha_char_count:
-        if c.isalpha():
-            print(f"{c}: {characters_dict[c]}")
-        else:
-            pass
-    print("============= END ===============")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+        text = get_book_text(book_path)
+        num_words = get_word_count(text)
+        characters_dict = get_characters_count(text)
+        alpha_char_count = sort_alpha_char_count(characters_dict)
+        
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {book_path}...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        for c in alpha_char_count:
+            if c.isalpha():
+                print(f"{c}: {characters_dict[c]}")
+            else:
+                pass
+        print("============= END ===============")
 
 
 def get_book_text(path):
